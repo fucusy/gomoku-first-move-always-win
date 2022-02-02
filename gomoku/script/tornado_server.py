f2729fa64cea1d8cb3e835bc146b463be6b727b9
qiangchen <fucus@qq.com>
2022-02-02 19:01:29 +0800

    hash is not necessary

--- gomoku/script/tornado_server.py
+++ gomoku/script/tornado_server.py
@@ -4,13 +4,14 @@ import sys
 
 import tornado.ioloop
 from divided_solution_manager import apply_all_transformation_for_steps_str, apply_transformation
-from divided_solution_manager import get_all_step_str2action, normalize_steps_str, steps2board_str, board_str_hash
+from divided_solution_manager import get_all_step_str2action, normalize_steps_str, steps2board_str
 from tornado import options
 from tornado import web
 from tornado.log import app_log
 
 options.options["log_file_prefix"] = "tornado_log"
 options.parse_command_line()
+
 
 class MainHandler(tornado.web.RequestHandler):
 
@@ -23,10 +24,10 @@ class MainHandler(tornado.web.RequestHandler):
         new_format_steps, trans = apply_all_transformation_for_steps_str(steps_url)
         next_move = ""
         for steps, tran in zip(new_format_steps, trans):
-            norm_steps = board_str_hash(steps2board_str(steps))
-            if norm_steps in board_str_hash2action:
+            norm_steps = steps2board_str(steps)
+            if norm_steps in board_str2action:
                 app_log.info("Find from saved step_str2action")
-                before_trans = board_str_hash2action[norm_steps]
+                before_trans = board_str2action[norm_steps]
                 tran_action = tran
                 next_move = apply_transformation(before_trans, tran, reverse=True)
                 break
@@ -35,13 +36,13 @@ class MainHandler(tornado.web.RequestHandler):
             x = ord(next_move[0]) - ord('a')
             y = int(next_move[1:]) - 1
             response = '{"input": "%s", "cache_count": %s, "x": %s, "y": %s, "before_trans": "%s", "trans_action": "%s"}' % \
-                       (steps_url, len(board_str_hash2action), x, y, before_trans, tran_action)
+                       (steps_url, len(board_str2action), x, y, before_trans, tran_action)
 
         else:
             timeout_program = "gtimeout"
             if platform.system() == 'Linux':
                 timeout_program = 'timeout'
-            cmd = '%s 5s %s/web_search %s' % (timeout_program, os.getcwd(), steps_url)
+            cmd = 'export LD_LIBRARY_PATH=/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH&&%s 5s %s/web_search %s' % (timeout_program, os.getcwd(), steps_url)
             app_log.info("Find from running the program: %s" % cmd)
             res = os.popen(cmd).read()
             response = res
@@ -62,9 +63,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         print("DEBUG MODE, Server Web UI Here")
         debug = True
-    global board_str_hash2action
-    board_str_hash2action = get_all_step_str2action()
-    print("cache step_str2action size ", len(board_str_hash2action))
+    global board_str2action
+    board_str2action = get_all_step_str2action()
+    print("cache step_str2action size ", len(board_str2action))
 
     app = make_app(debug)
     app.listen(port)
