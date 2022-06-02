bddfe115d570e0ac205b52e8157cbe7f289e4965
chenqiang <fucus@qq.com>
2022-06-03 07:41:48 +0800

    add plans

---
+++ second_move_all_solution_for_far_step1_day2_part1.sh
@@ -0,0 +1,7 @@
+./problem_comb.sh h8_f5 i9
+./problem_comb.sh h8_f4 i9
+./problem_comb.sh h8_f3 i9
+./problem_comb.sh h8_f2 i9
+./problem_comb.sh h8_f1 i9
+./problem_comb.sh h8_e5 i9
+./problem_comb.sh h8_e4 i9
