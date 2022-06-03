c333e54a37c24a872b2ca6d1aea2fb1a06364505
qiangchen <fucus@qq.com>
2022-06-03 12:35:38 +0800

    write into the db

--- gomoku/script/write_board2action_into_db.py
+++ gomoku/script/write_board2action_into_db.py
@@ -9,10 +9,10 @@ board2action = get_all_step_str2action()
 i = 0
 n = len(board2action)
 for key, value in board2action.items():
-    #db.put(str.encode(key), str.encode(value))
-    db.get(str.encode(key))
+    db.put(str.encode(key), str.encode(value))
+    #db.get(str.encode(key))
     i +=  1
     if i % 10000 == 0:
-        print(db.get(str.encode(key)))
+        #print(db.get(str.encode(key)))
         print(i, n)
         print(i/n)
